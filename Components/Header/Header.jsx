808e7fa20da04de83a6e63239454ad01f622a352
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-01-03 03:01:15 +0600

    style added

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -4,30 +4,7 @@ const Header = () => {
   return (
     <section>
       <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label
-              htmlFor="my-drawer"
-              tabIndex={0}
-              className="btn btn-ghost btn-circle"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </label>
-            <div className="drawer-content"></div>
-          </div>
+        <div className="navbar-start">          
           <Link href="/">
             <span className="btn btn-ghost normal-case text-xl" id="link">
               VsTube
